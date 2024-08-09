72a15f01e9d2d26e56c2d3c27291aad2810a6202
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-08-09 15:37:30 -0300

    style: adjustments to the responsiveness of the project

--- src/components/Ranking/CountryItem/TotalMedals/index.tsx
+++ src/components/Ranking/CountryItem/TotalMedals/index.tsx
@@ -7,8 +7,12 @@ interface TotalMedalsProps {
 export function TotalMedals({ quantity }: TotalMedalsProps) {
     return (
         <div className="flex items-center">
-            <img src={generalMedal} alt="Medalha" className="w-12 object-cover md:w-14 md:hidden" />
-            <p className="ml-2 mr-4 text-midnight-blue text-xl md:mx-0 md:text-2xl lg:text-3xl">
+            <img
+                src={generalMedal}
+                alt="Medalha"
+                className="w-10 object-cover sm:w-12 md:w-14 md:hidden"
+            />
+            <p className="ml-2 mr-4 text-midnight-blue text-sm sm:text-xl md:mx-0 md:text-2xl lg:text-3xl">
                 = {quantity}
             </p>
         </div>
